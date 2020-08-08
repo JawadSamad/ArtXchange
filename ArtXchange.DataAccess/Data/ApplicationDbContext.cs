e7f6bf6d69a7031f29a203a987c73392555b4d2a
Jawad Samad <J.awads@hotmail.com>
2020-08-08 15:19:09 +0200

    Added DB and added a few UI changes

--- ArtXchange.DataAccess/Data/ApplicationDbContext.cs
+++ ArtXchange.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ArtXchange.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace ArtXchange.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
