b6a958e06253100249213e4b3cea4efb1f7a286a
Jawad Samad <J.awads@hotmail.com>
2020-08-10 12:38:54 +0200

    Implemented and added repositories

---
+++ ArtXchange.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using ArtXchange.DataAccess.Data;
+using ArtXchange.DataAccess.Repository.IRepository;
+using ArtXchange.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtXchange.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
