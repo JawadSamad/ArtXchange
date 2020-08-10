b550e59d237996d9308ba867c2f69bac7da89613
Jawad Samad <J.awads@hotmail.com>
2020-08-10 15:18:05 +0200

    Added "Create, Edit and Delete" functionality

--- ArtXchange.DataAccess/Repository/CategoryRepository.cs
+++ ArtXchange.DataAccess/Repository/CategoryRepository.cs
@@ -23,7 +23,6 @@ namespace ArtXchange.DataAccess.Repository
             if(objFromDb != null)
             {
                 objFromDb.Name = category.Name;
-                _db.SaveChanges();
             }
         }
     }
--- ArtXchange/Areas/Admin/Controllers/CategoryController.cs
+++ ArtXchange/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ArtXchange.DataAccess.Repository.IRepository;
+using ArtXchange.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtXchange.Areas.Admin.Controllers
@@ -23,13 +24,64 @@ namespace ArtXchange.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                // THis is for create
+                return View(category);
+            }
+            //this is for editing
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
         #region API Calls
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
 
         [HttpGet]
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleted" });
         }
 
         #endregion
